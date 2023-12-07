40029a249445f208bf536cdde08b62e5fe9544f2
Ziyaddin <ziya.huseynov2001@mail.ru>
2023-12-08 01:54:00 +0400

    3 Hisse Formalar elave eledim

---
+++ marketWebSite/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace marketWebSite.Models
+{
+    public class Contact
+    {
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+
+    }
+}
